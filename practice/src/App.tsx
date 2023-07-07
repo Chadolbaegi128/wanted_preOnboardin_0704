1591a734dfe1310e1637c69a3bbda8d201ba2895
YeonjunJung <duswns12893@gmail.com>
2023-07-08 05:01:39 +0900

    feat: 과제 프로젝트 완성

--- practice/src/App.tsx
+++ practice/src/App.tsx
@@ -5,9 +5,11 @@ import { About } from "./pages/About";
 
 export default function App() {
   return (
-    <Router>
-      <Route path="/" component={<Root />} />
-      <Route path="/about" component={<About />} />
-    </Router>
+    <>
+      <Router>
+        <Route path="/" component={<Root />} />
+        <Route path="/about" component={<About />} />
+      </Router>
+    </>      
   );
 }
