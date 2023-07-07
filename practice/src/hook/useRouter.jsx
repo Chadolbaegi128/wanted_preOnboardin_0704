cf2d7bfa9091485f8c1776db1b39306355b51f5f
YeonjunJung <duswns12893@gmail.com>
2023-07-08 03:33:56 +0900

    fix: 과제 프로젝트 파일 삭제

--- practice/src/hook/useRouter.jsx
+++
@@ -1,9 +0,0 @@
-export const useRouter = () => {
-    const push = path => {
-        console.log(path);
-        window.history.pushState({}, "", path);
-        window.EventTarget.dispatchEvent(new PopStateEvent("popstate"));
-    }
-
-    return {push};
-}
